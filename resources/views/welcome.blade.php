db057d832d955426a5e78c15a14a116071ab2115
Umeshww <kashishverm1910@gmail.com>
2026-07-04 19:58:32 +0530

    FFF

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,7 +49,12 @@
                 </nav>
             @endif
         </header>
-        
+        <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
+            <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
+                
+             <h1>Start New Lover</h1>
+            </main>
+        </div>
 
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
